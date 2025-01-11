1312ffb1b251711c390acab16ef15545c5663302
m88deng <melideng123@gmail.com>
2025-01-11 17:19:54 -0500

    Navigation

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import HomePage from './pages/HomePage';
 import Artworks from './components/illustration/Artworks';
 import EeilCitationPage from './pages/EeilCitationPage';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
-
+      <ScrollToTop />
       <Hero />
       <Routes>
         <Route path="/" element={<HomePage />} />
